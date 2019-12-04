c22108cdddcacd82b617ce9181061ecc1bad63ed
ameliaMagdalena <56532583+ameliaMagdalena@users.noreply.github.com>
2019-12-05 01:33:21 +0700

    ....

--- app/src/main/java/com/example/dogeeku/ui/share/ShareFragment.java
+++ app/src/main/java/com/example/dogeeku/ui/share/ShareFragment.java
@@ -1,16 +1,22 @@
 package com.example.dogeeku.ui.share;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import com.example.dogeeku.R;
+import com.example.dogeeku.ui.home.LayananKlinikFragment;
 
 public class ShareFragment extends Fragment {
 
@@ -21,13 +27,33 @@ public class ShareFragment extends Fragment {
         shareViewModel =
                 ViewModelProviders.of(this).get(ShareViewModel.class);
         View root = inflater.inflate(R.layout.fragment_share, container, false);
-        final TextView textView = root.findViewById(R.id.text_share);
-        shareViewModel.getText().observe(this, new Observer<String>() {
+
+
+       ImageButton facebook = (ImageButton) root.findViewById(R.id.facebook);
+        ImageButton instagram = (ImageButton) root.findViewById(R.id.instagram);
+
+        facebook.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_VIEW);
+                i.addCategory(Intent.CATEGORY_BROWSABLE);
+                i.setData(Uri.parse("https://developer.android.com/guide/topics/resources/string-resource?hl=id"));
+                startActivity(i);
             }
         });
+
+        instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_VIEW);
+                i.addCategory(Intent.CATEGORY_BROWSABLE);
+                i.setData(Uri.parse("https://developer.android.com/guide/topics/resources/string-resource?hl=id"));
+                startActivity(i);
+            }
+        });
+
         return root;
     }
 }
