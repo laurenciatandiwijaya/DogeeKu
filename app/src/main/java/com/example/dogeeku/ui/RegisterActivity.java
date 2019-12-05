373cc8090a29091afafdbe61d8e5b939e8850797
laurenciatandiwijaya <43399044+laurenciatandiwijaya@users.noreply.github.com>
2019-12-05 13:52:03 +0700

    login register logout munculin nama

--- app/src/main/java/com/example/dogeeku/ui/RegisterActivity.java
+++ app/src/main/java/com/example/dogeeku/ui/RegisterActivity.java
@@ -2,27 +2,27 @@ package com.example.dogeeku.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import com.example.dogeeku.DatabaseAccess;
 import com.example.dogeeku.DatabaseOpenHelper;
 import com.example.dogeeku.MainActivity;
+import com.example.dogeeku.SessionManager;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.dogeeku.R;
-
 import android.app.AppComponentFactory;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.ContactsContract;
+import java.util.HashMap;
+import android.app.Activity;
+import android.text.Html;
 
 public class RegisterActivity extends AppCompatActivity {
     protected Cursor cursor;
@@ -30,12 +30,19 @@ public class RegisterActivity extends AppCompatActivity {
     Button button;
     EditText nama_lengkap, email, password;
 
+    // Session Manager Class
+    SessionManager session;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Session class instance
+        session = new SessionManager(getApplicationContext());
+
         dbHelper = new DatabaseOpenHelper(this);
         nama_lengkap = (EditText) findViewById(R.id.nama_lengkap);
         email = (EditText) findViewById(R.id.email);
@@ -63,7 +70,7 @@ public class RegisterActivity extends AppCompatActivity {
                 String passwordDB = password.getText().toString();
                 databaseAccess.insert(namaDB, emailDB, passwordDB);
 
-                Intent register = new Intent (getApplicationContext(), MainActivity.class);
+                Intent register = new Intent (getApplicationContext(), LoginActivity.class);
                 startActivity(register);
             }
         });
